bc12c50cc98d459c80db7529a91a7d74078cd3e1
Harmanjit Singh <harmanjit140500@gmail.com>
2020-09-13 03:41:06 +0530

    initial commit

---
+++ lib/colorFile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorFile {
+  Color charcoal = Color(0xff264653);
+  Color persianGreen = Color(0xff2a9d8f);
+  Color orangeYellow = Color(0xffe9c46a);
+  Color sandyBrown = Color(0xfff4a261);
+  Color burntRed = Color(0xfffE76F51);
+}
---
+++ lib/firebaseDatabase.dart
@@ -0,0 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseDatabase {}
