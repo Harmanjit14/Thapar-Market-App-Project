14e0ee89f2e76491de01d24834b21bad5305c08f
Harmanjit Singh <harmanjit140500@gmail.com>
2020-09-13 17:10:28 +0530

    login/register Button added

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -23,8 +23,28 @@ class _HomeScreenState extends State<HomeScreen> with ColorFile {
                 child: Container(
                   padding: EdgeInsets.all(20),
                   child: Column(children: [
-                    Container(
-                      child: Text('Thapar Market'),
+                    Expanded(
+                      flex: 4,
+                      child: Container(child: Image.asset('lib/assets/1.png')),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Container(
+                            child: Text(
+                              'Thapar Market',
+                              style: TextStyle(
+                                  color: orangeYellow,
+                                  fontSize: 37,
+                                  fontFamily: 'Maven',
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 1.3),
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                        ],
+                      ),
                     ),
                   ]),
                 ),
@@ -40,6 +60,9 @@ class _HomeScreenState extends State<HomeScreen> with ColorFile {
                 width: MediaQuery.of(context).size.width,
                 color: charcoal,
                 child: Container(
+                  child: Column(children: [
+                    allButtons(context),
+                  ]),
                   width: MediaQuery.of(context).size.width,
                   decoration: BoxDecoration(
                     color: Colors.white,
@@ -54,4 +77,50 @@ class _HomeScreenState extends State<HomeScreen> with ColorFile {
       )),
     );
   }
+}
+
+Widget loginButtton() {}
+
+Widget regisButton() {}
+
+Widget allButtons(BuildContext context) {
+  ColorFile obj = new ColorFile();
+  return Container(
+    margin: EdgeInsets.all(20),
+    child: Column(children: [
+      Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width * 0.5,
+        child: RaisedButton(
+          child: Text(
+            'Login',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          padding: EdgeInsets.all(0),
+          onPressed: () {},
+          elevation: 7,
+          color: obj.burntRed,
+        ),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      Container(
+        height: 60,
+        width: MediaQuery.of(context).size.width * 0.5,
+        child: RaisedButton(
+          child: Text(
+            'Register',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          padding: EdgeInsets.all(0),
+          onPressed: () {},
+          elevation: 7,
+          color: obj.burntRed,
+        ),
+      ),
+    ]),
+  );
 }
