ba64ce7ca20008409a9e85650cae5ea827de18e7
Harmanjit Singh <harmanjit140500@gmail.com>
2020-09-14 09:53:07 +0530

    sidebar DONE

--- lib/mainScreen.dart
+++ lib/mainScreen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+import 'package:rxdart/rxdart.dart';
 import 'package:TMart/colorFile.dart';
 import 'package:flutter/material.dart';
 
@@ -31,13 +31,31 @@ class SideNavBar extends StatefulWidget {
 
 class _SideNavBarState extends State<SideNavBar>
     with SingleTickerProviderStateMixin<SideNavBar>, ColorFile {
+
   AnimationController animationController;
   StreamController<bool> sideBarController;
+  Stream<bool> isSideBarOpenedStream;
   final duration = Duration(milliseconds: 500);
-  bool isBarOpen = false;
+  StreamSink<bool> isSidebarOpenedSink;
+
+  void onIconPressed() {
+    final animationStatus = animationController.status;
+    final isAnimationComplete = animationStatus == AnimationStatus.completed;
+    if (isAnimationComplete) {
+      isSidebarOpenedSink.add(false);
+      animationController.reverse();
+    } else {
+      isSidebarOpenedSink.add(true);
+      animationController.forward();
+    }
+  }
+
   @override
   void initState() {
     animationController = AnimationController(vsync: this, duration: duration);
+    sideBarController = PublishSubject<bool>();
+    isSideBarOpenedStream = sideBarController.stream;
+    isSidebarOpenedSink = sideBarController.sink;
     super.initState();
   }
 
@@ -45,37 +63,49 @@ class _SideNavBarState extends State<SideNavBar>
   void dispose() {
     animationController.dispose();
     sideBarController.close();
+    isSidebarOpenedSink.close();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedPositioned(
-      duration: duration,
-      top: 0,
-      bottom: 0,
-      left: isBarOpen ? 0 : 0,
-      right: isBarOpen ? 0 : MediaQuery.of(context).size.width - 45,
-      child: Row(children: [
-        Expanded(
-            child: Container(
-          color: charcoal,
-        )),
-        Align(
-          alignment: Alignment(0, -0.8),
-          child: Container(
-            alignment: Alignment.centerLeft,
-            height: 110,
-            width: 35,
-            color: charcoal,
-            child: AnimatedIcon(
-                icon: AnimatedIcons.menu_close,
-                size: 25,
-                color: orangeYellow,
-                progress: animationController.view),
-          ),
-        )
-      ]),
+    return StreamBuilder<bool>(
+      initialData: false,
+      stream: isSideBarOpenedStream,
+      builder: (context, isSiderbarOpenAsync) {
+        return AnimatedPositioned(
+          duration: duration,
+          top: 0,
+          bottom: 0,
+          left: isSiderbarOpenAsync.data ? 0 : 0,
+          right:isSiderbarOpenAsync.data ? 0 : MediaQuery.of(context).size.width - 45,
+          child: Row(children: [
+            Expanded(
+                child: Container(
+              color: charcoal,
+            )),
+            Align(
+              alignment: Alignment(0, -0.8),
+              child: GestureDetector(
+                onTap: (){
+                  onIconPressed();
+                },
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  height: 110,
+                  width: 35,
+                  color: charcoal,
+                  child: AnimatedIcon(
+                      icon: AnimatedIcons.menu_close,
+                      size: 25,
+                      color: orangeYellow,
+                      progress: animationController.view),
+                ),
+              ),
+            )
+          ]),
+        );
+      },
     );
   }
 }
